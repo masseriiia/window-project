f359c575b93917ef7878f60bb0fa6ba13d7d22b0
Danil <bloodshoww@mail.ru>
2023-12-05 18:04:53 +0300

    fix: renamed the classes and nested object as argument

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,17 @@ import './modules/tabs'
 import tabs from "./modules/tabs";
 
 window.addEventListener('DOMContentLoaded', () => {
-    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active')
-    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click')
+    tabs({
+        headerSelector: '.glazing_slider',
+        tabSelector: '.glazing_block',
+        contentSelector:'.glazing_content',
+        activeClass: 'active'
+    })
+    tabs({
+        headerSelector: '.decoration_slider',
+        tabSelector: '.no_click',
+        contentSelector: '.decoration_content > div > div',
+        activeClass: 'after_click'
+    })
 })
 
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,20 +1,20 @@
-const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+const tabs = ({headerSelector, tabSelector, contentSelector, activeClass}) => {
     const header = document.querySelector(headerSelector);
-    const tab = document.querySelectorAll(tabSelector);
-    const content = document.querySelectorAll(contentSelector);
+    const tabs = document.querySelectorAll(tabSelector);
+    const contents = document.querySelectorAll(contentSelector);
 
     const hideTabContent = () => {
-        content.forEach(item => {
+        contents.forEach(item => {
             item.style.display = 'none'
         })
-        tab.forEach(item => {
+        tabs.forEach(item => {
             item.classList.remove(activeClass)
         })
     }
 
     const showTabContent = (i = 0) => {
-        content[i].style.display = 'block'
-        tab[i].classList.add(activeClass)
+        contents[i].style.display = 'block'
+        tabs[i].classList.add(activeClass)
     }
 
     hideTabContent()
@@ -24,7 +24,7 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
         const target = e.target
         if ( target && target.classList.contains(tabSelector.replace(/\./, "")) ||
             target.parentNode.classList.contains(tabSelector.replace(/\./, ""))) {
-            tab.forEach((item, i) => {
+            tabs.forEach((item, i) => {
                 if (target == item || target.parentNode == item) {
                     hideTabContent();
                     showTabContent(i)
