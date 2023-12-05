0b95ad8b4e20d44e67a3f3db215c51e4107abd8e
Danil <bloodshoww@mail.ru>
2023-12-05 19:09:42 +0300

    fix: resolve conflicts

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+import './slider'
+import './modules/tabs'
+import tabs from "./modules/tabs";
+import modals from "./modules/modals";
+
+window.addEventListener('DOMContentLoaded', () => {
+    modals()
+    tabs({
+        headerSelector: '.glazing_slider',
+        tabSelector: '.glazing_block',
+        contentSelector:'.glazing_content',
+        activeClass: 'active'
+    })
+    tabs({
+        headerSelector: '.decoration_slider',
+        tabSelector: '.no_click',
+        contentSelector: '.decoration_content > div > div',
+        activeClass: 'after_click'
+    })
+})
+
+
