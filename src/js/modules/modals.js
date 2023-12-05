8a80df4bd6e30efdef857543c3868e1543024549
Danil <bloodshoww@mail.ru>
2023-12-05 16:33:00 +0300

    fix: changed the name and made a separate function for closing the modal window

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,36 +1,41 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
-        const trigger = document.querySelectorAll(triggerSelector)
+    const bindModal = (triggerSelector, modalSelector, closeSelector) => {
+        const triggers = document.querySelectorAll(triggerSelector)
         const close = document.querySelector(closeSelector)
         const modal = document.querySelector(modalSelector)
 
-        trigger.forEach(item => {
+        const closeModal = () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+
+        triggers.forEach(item => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault()
                 }
                 modal.style.display = "block";
-                // document.body.classList.add('modal-open')
                 document.body.style.overflow = "hidden"
             })
         })
 
         close.addEventListener('click', () => {
-            modal.style.display = "none";
-            // document.body.classList.remove('modal-open')
-            document.body.style.overflow = "";
+            closeModal()
         })
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
-                modal.style.display = "none";
-                // document.body.classList.remove('modal-open')
-                document.body.style.overflow = "";
+                closeModal()
+            }
+        })
+        document.addEventListener('keydown', (e) => {
+            if (e.code === "Escape") {
+                closeModal()
             }
         })
     }
 
-    function showModalByTime(selector, time) {
-        setTimeout(function () {
+    const showModalByTime = (selector, time) => {
+        setTimeout( () => {
             document.querySelector(selector).style.display = 'block'
             document.body.style.overflow = 'hidden'
         }, time)
