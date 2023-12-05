320876bb46934037ede0588235c6c4aaa889206f
Danil <bloodshoww@mail.ru>
2023-12-05 14:18:03 +0300

    feat: add tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+import './slider'
+import './modules/tabs'
+import tabs from "./modules/tabs";
+
+window.addEventListener('DOMContentLoaded', () => {
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active')
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click')
+})
+
