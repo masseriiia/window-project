ab0b5f0b22333d167f4730e662dfa282093a1608
Danil <bloodshoww@mail.ru>
2023-12-11 22:18:07 +0300

    fix: changed receiving photos in public

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,53 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import VitePluginBrowserSync from 'vite-plugin-browser-sync';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import htmlMinifier from 'vite-plugin-html-minifier';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    publicPath: "/dist",
-    resolve: {
-        extensions: [".js", ".tsx", ".ts", ".json", ".svg", ".png", ".jpg", ".jpeg"],
+    server: {
+        open: 'index.html',
+    },
+
+    plugins: [
+        react(),
+        VitePluginBrowserSync({
+            bs: {
+                ui: {
+                    port: 8080,
+                },
+                notify: true,
+            },
+        }),
+        ViteImageOptimizer({}),
+        htmlMinifier({
+            minify: true,
+        }),
+    ],
+
+    build: {
+        outDir: 'public', // Изменено на 'public' для сохранения билда в эту папку
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'assets/js/[name].js',
+                entryFileNames: 'assets/js/[name].js',
+                assetFileNames: ({ name }) => {
+                    if (/\.(gif|jpe?g|png|svg|webp)$/.test(name ?? '')) {
+                        return 'assets/img/[name][extname]';
+                    }
+
+                    if (/\.css$/.test(name ?? '')) {
+                        return 'assets/css/[name][extname]';
+                    }
+
+                    if (/\.(woff(2)?|ttf|eot|svg)$/.test(name ?? '')) {
+                        return 'assets/fonts/[name][extname]';
+                    }
+
+                    return 'assets/[name][extname]';
+                },
+            },
+        },
     },
 });
